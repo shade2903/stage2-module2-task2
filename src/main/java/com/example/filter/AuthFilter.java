2280ae990d162009b7559cba786af458c7465498
shade2903 <nighshade2903@gmail.com>
2023-05-17 16:31:34 +0300

    refactoring

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,33 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/user/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        if(httpServletRequest.getSession().getAttribute("user") == null){
+            HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+            httpServletResponse.sendRedirect("/login.jsp");
+        }else{
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,48 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet
+public class LoginServlet extends HttpServlet {
+    final String  LOGIN_JSP = "/login.jsp";
+    final String HELLO_JSP = "/user/hello.jsp";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect(LOGIN_JSP);
+        } else {
+            resp.sendRedirect(HELLO_JSP);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        if(password.trim().equals("")){
+            RequestDispatcher rq = req.getRequestDispatcher(LOGIN_JSP);
+            rq.forward(req, resp);
+        }
+        List<String> users = Users.getInstance().getUsers();
+        for (String e : users) {
+            if (e.equals(login)) {
+                req.getSession().setAttribute("user", login);
+            }
+            if (req.getSession().getAttribute("user") != null) {
+                RequestDispatcher rq = req.getRequestDispatcher(HELLO_JSP);
+                rq.forward(req, resp);
+            }
+        }
+        resp.sendRedirect(LOGIN_JSP);
+
+    }
 }
